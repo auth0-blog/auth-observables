e2cf02c3a1e7a22b9f34fed7e046aac692e7f3de
Ryan Chenkie <ryanchenkie@gmail.com>
2015-09-09 23:07:55 -0600

    Added custom operators

--- client/app.js
+++ client/app.js
@@ -1,5 +1,55 @@
 (function() {
 
+  Rx.Observable.prototype.authenticate = function(credentials) {
+
+    // We need a function to handle the fetch request for the JWT
+    function getJwt(credentials) {
+
+      if(credentials) {
+        var formattedCreds = "username=" + credentials.username + "&password=" + credentials.password;
+
+        // We need some config to specify that this is to be a POST request
+        // We set the content type and put the passed-in credentials as the body
+        var config = {
+          method: 'POST',
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: formattedCreds
+        }
+
+        return fetch(credentials.loginPath, config).then(function(data) {
+          return data.json().then(function(jwt) {
+            localStorage.setItem('id_token', jwt.id_token);
+          });
+        });        
+      }
+    }    
+
+    return this.flatMap(function(credentials) {
+      return Rx.Observable.fromPromise(getJwt(credentials));
+    });
+  }
+
+  Rx.Observable.prototype.authenticated = function(route) {
+
+    var jwt = localStorage.getItem('id_token');
+    
+    if(jwt != undefined && jwt != null) {
+      return this.map(function() {
+        return {
+          route: route,
+          jwt: jwt
+        }
+      });
+    }
+    else {
+      return this.map(function() {
+        return {
+          route: route
+        }
+      });
+    }
+  }
+
   // Selectors for our buttons
   var loginButton = document.querySelector('#login-button');  
   var quoteButton = document.querySelector('#quote-button');
@@ -16,92 +66,66 @@
   // map a string that contains values from the username and password input boxes
   var loginStream = loginClickStream
     .map(function() {
+      var loginPath = 'http://localhost:3001/sessions/create';
       var username = document.querySelector('#username').value;
       var password = document.querySelector('#password').value;
-      return "username=" + username + "&password=" + password;
-    });
-
-  // We need a stream for getting the JWT from the server which will
-  // require us to pass in credentials 
-  var getJwtStream = loginStream
-    // flatMap flattens the new stream such that we can get access
-    // to JSON data returned from the fetch operation
-    .flatMap(function(credentials) {
-      // We can easily turn a promise into an observable with fromPromise
-      return Rx.Observable.fromPromise(getJwt(credentials))
+      return {
+        loginPath: loginPath,
+        username: username,
+        password: password
+      }
+    })
+    .authenticate(function(credentials) {
+      return credentials;
     });
 
   // Here we want to map the endpoint for where we'll get our public quotes
   var quoteStream = quoteClickStream
-    .map({url: 'http://localhost:3001/api/random-quote'});
+    .map(function() {
+      return {
+        route: 'http://localhost:3001/api/random-quote'
+      }
+    });
 
   // We also need a separate stream for the secret quotes and in this
   // case we need to include the JWT from local storage
   var secretQuoteStream = secretQuoteClickStream
-    .map(function() {
-      var jwt = localStorage.getItem('id_token');
-      return {
-        url: 'http://localhost:3001/api/protected/random-quote',
-        jwt: jwt
-      }
-    });  
+    .authenticated('http://localhost:3001/api/protected/random-quote');  
 
   // We need a response stream that handles the fetch operation
   var quoteResponseStream = quoteStream
-    .flatMap(function(request) {
-      return Rx.Observable.fromPromise(fetchQuote(request));
+    .flatMap(function(url) {
+      return Rx.Observable.fromPromise(fetchQuote(url));
     })
     // Instead of setting up a separate stream for the secret quotes, we
     // can simply merge the secretQuoteStream in and apply the same flatMap
     .merge(secretQuoteStream
-      .flatMap(function(request) {
-        return Rx.Observable.fromPromise(fetchQuote(request));
+      .flatMap(function(data) {
+        return Rx.Observable.fromPromise(fetchQuote(data));
       })
-    );
-
-  // We need a function to handle the fetch request for the JWT
-  function getJwt(credentials) {
-
-    // We need some config to specify that this is to be a POST request
-    // We set the content type and put the passed-in credentials as the body
-    var config = {
-      method: 'POST',
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: credentials
-    }
-
-    return fetch('http://localhost:3001/sessions/create', config).then(function(data) {
-      return data.json().then(function(jwt) {
-        return jwt;
-      });
-    });
-  }
+    );  
 
   // One fetch function will handle getting both the public and private quotes 
-  function fetchQuote(request) {
+  function fetchQuote(data) {
     // We just need to check whether a JWT was sent in as part of the
     // config object. If it was, we'll include it as an Authorization header
-    if(request.jwt) {
+    if(data.jwt) {
       var config = {
         headers: {
-          "Authorization": "Bearer " + request.jwt
+          "Authorization": "Bearer " + data.jwt
         }
       }
     }
 
-    return fetch(request.url, config).then(function(data) {
+    return fetch(data.route, config).then(function(data) {
       return data.text().then(function(text) {
         return text;
       });
     });
   }
-  
-  // Finally, we need to subscribe to the streams to observe events as
-  // they happen. We can observe the getJwtStream for returned JWTs and
-  // set them in local storage
-  getJwtStream.subscribe(function(jwt) {
-    localStorage.setItem('id_token', jwt.id_token);
-  });
+
+  // Finally, we need to subscribe to the streams to observe events as they happen.
+  loginStream.subscribe();
 
   // We subscribe to the quoteResponseStream, listen
   // for quotes that get returned, and put them on the page
